ed5e853d7efcc201a415d927df5d12a82d5910bf
Sasidhar <9908135565ks@gmail.com>
2025-11-08 00:23:41 +0530

    Initial Commit

---
+++ src/main/java/com/aits/mobileprepaid/service/RechargePlanService.java
@@ -0,0 +1,33 @@
+package com.aits.mobileprepaid.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
+
+import com.aits.mobileprepaid.entity.RechargePlan;
+import com.aits.mobileprepaid.repo.RechargePlanRepository;
+
+	@Service
+	public class RechargePlanService {
+		
+		@Autowired
+	    private  RechargePlanRepository repository;
+	
+	    
+	
+	    public List<RechargePlan> getAllPlans() {
+	        return repository.findAll();
+	    }
+	
+	    public List<RechargePlan> getPlansByCategory(String category) {
+	        return repository.findByCategory(category);
+	    }
+	
+		public RechargePlan insert(@NonNull RechargePlan rechargeplan) {
+			// TODO Auto-generated method stub
+			
+			return repository.save(rechargeplan);
+		}
+	}
